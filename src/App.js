b5c34b1f927c68efe84600a29af377fe39314700
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-12 17:32:39 +0530

    implemented Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:imdbId" element={<MovieDetail />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
