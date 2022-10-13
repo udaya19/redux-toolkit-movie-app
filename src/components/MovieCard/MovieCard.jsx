f8f80cb25feab1b4f70db3d84413866468b1a753
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-13 11:45:49 +0530

    get all shows

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import "./MovieCard.scss";
+import { Link } from "react-router-dom";
 const MovieCard = (props) => {
   const { data } = props;
   return (
     <div className="card-item">
-      <div className="card-inner">
-        <div className="card-top">
-          <img
-            src="https://s.studiobinder.com/wp-content/uploads/2017/12/Movie-Poster-Template-Dark-with-Image.jpg?x81279"
-            alt="movie-poster"
-          />
-        </div>
-        <div className="card-bottom">
-          <div className="card-info">
-            <h4>{data.Title}</h4>
-            <p>{data.Year}</p>
+      <Link to={`/movie/${data.imdbId}`}>
+        <div className="card-inner">
+          <div className="card-top">
+            <img
+              src="https://s.studiobinder.com/wp-content/uploads/2017/12/Movie-Poster-Template-Dark-with-Image.jpg?x81279"
+              alt="movie-poster"
+            />
+          </div>
+          <div className="card-bottom">
+            <div className="card-info">
+              <h4>{data.Title}</h4>
+              <p>{data.Year}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
   console.log(movies);
-  let renderMovies = "";
+  let renderMovies,
+    renderShows = "";
   renderMovies =
     movies.Response === "True" ? (
       movies.Search.map((movie, index) => {
@@ -18,11 +20,25 @@ const MovieListing = () => {
         <h3>{movies.error}</h3>
       </div>
     );
+  renderShows =
+    shows.Response === "True" ? (
+      shows.Search.map((show, index) => {
+        return <MovieCard key={index} data={show} />;
+      })
+    ) : (
+      <div className="movies-error">
+        <h3>{shows.error}</h3>
+      </div>
+    );
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="movie-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   );
