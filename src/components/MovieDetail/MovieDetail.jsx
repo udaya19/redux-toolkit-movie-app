5646a2f219e2df3cb47784b7f127f29edae90dbc
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-13 12:30:20 +0530

    created movie details page with html and css

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -13,7 +13,47 @@ const MovieDetail = () => {
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
   }, [dispatch, imdbID]);
-  return <div>MovieDetail</div>;
+  return (
+    <div>
+      <div className="movie-section">
+        <div className="section-left">
+          <div className="movie-title">{data.Title}</div>
+          <div className="movie-ratin">
+            <span>IMDB rating:{data.imdbRating}</span>
+            <span>IMDB votes:{data.Votes}</span>
+            <span>Run time:{data.Runtime}</span>
+            <span>Year:{data.Year}</span>
+          </div>
+          <div className="movie-plot">{data.Plot}</div>
+          <div className="movie-info">
+            <div>
+              <span>Director</span>
+              <span>{data.Director}</span>
+            </div>
+            <div>
+              <span>Stars</span>
+              <span>{data.Actors}</span>
+            </div>
+            <div>
+              <span>Genres</span>
+              <span>{data.Genre}</span>
+            </div>
+            <div>
+              <span>Languages</span>
+              <span>{data.Language}</span>
+            </div>
+            <div>
+              <span>Awards</span>
+              <span>{data.Awards}</span>
+            </div>
+          </div>
+        </div>
+        <div className="section-right">
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetail;
